3e70a3422611779bd59eb18ca203c8128b52b480
Mahmoud Hammad <0597694093mahmoud@gmail.com>
2022-06-13 02:21:10 +0300

    delete unnecessary comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
-import './index.css'
+import "./index.css";
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
