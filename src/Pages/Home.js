c0641d38e18a63daa0de215797fbd33a9eacae0f
Mahmoud Hammad <0597694093mahmoud@gmail.com>
2022-06-14 01:53:35 +0300

    modify the folders name

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Nav from "../../src/Components/Nav/Nav";
 import Hero from "../Components/HomePage/Hero/Hero";
+import Discount from "../Components/HomePage/Discount/Discount";
+import Products from "../Components/HomePage/Products/Products"
 import "../index.css";
 import { MainContainer } from "../Global.Style";
 import Categories from "../Components/HomePage/Categories/Categories";
@@ -11,6 +13,8 @@ function Home() {
         <Nav />
         <Hero />
         <Categories />
+        <Discount />
+        <Products />
       </MainContainer>
     </div>
   );
