19e4da18ea3d67a4224f19b271f8311ff9bb2e7c
Mahmoud Hammad <0597694093mahmoud@gmail.com>
2022-06-19 01:33:17 +0300

    add the nav two to the details page

---
+++ src/Pages/Details.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavTwo from "../Components/NavTwo/NavTwo";
+import ProductDetails from "../Components/ProductDetails/ProductDetails";
+
+function Details() {
+  return (
+    <div>
+      <NavTwo />
+      <ProductDetails />
+    </div>
+  );
+}
+
+export default Details;
