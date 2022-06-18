bee6d6dcd92f5625bd63e75271876baba2e84448
Mahmoud Hammad <0597694093mahmoud@gmail.com>
2022-06-18 20:58:17 +0300

    Merge branch 'main' into categories-page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
 import Home from "../src/Pages/Home";
+import LOGIN from "./Pages/LogIn";
 import SignUp from "./Pages/SignUp";
 import Categories from "./Pages/Categories";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
 function App() {
   return (
     <Router>
@@ -11,6 +13,7 @@ function App() {
       <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/signup" element={<SignUp />}/>
+      <Route path="/Login" element={<LOGIN />}/>
       </Routes>
     </div>
     </Router>
--- src/Components/Login/RightSection.js
+++ src/Components/Login/RightSection.js
@@ -6,19 +6,20 @@ import {
   StyledButton,
   Center,
   StyledParagraph,
+  FlexBox
 } from "../../Global.Style";
-import { Link } from "react-router-dom";
-
-import "./SignStyle.css";
+import "../SignPage/SignPage.Style"
 import { FaLongArrowAltRight } from "react-icons/fa";
 import { IconContext } from "react-icons";
+import { Link } from "react-router-dom";
+import "./Login.css"
 function RightSection() {
   return (
     <IconContext.Provider value={{ className: "react-icons" }}>
       <RightSectionSign>
         <div className="form-sec">
           <form action="" method="post">
-            <h3>SIGN UP</h3>
+            <h3>LOGIN</h3>
             <FlexBoxCol className="input-sec email-input">
               <label htmlFor="">&#9993; Email</label>
               <Input type="email" placeholder="John@example.com;" />
@@ -26,27 +27,22 @@ function RightSection() {
             <FlexBoxCol className="input-sec">
               <label htmlFor=""></label>
               <Input type="password" placeholder="  &#128274; Password" />
+              <button className="butn_forget" >FORGET?</button>
+    
             </FlexBoxCol>
-            <FlexBoxCol className="input-sec">
-              <label htmlFor=""></label>
-              <Input
-                type="password"
-                placeholder="  &#128274; Confirm Password"
-              />
-            </FlexBoxCol>
+
             <StyledButton color="#707070">
               <Center>
-                SIGN UP
+              LOGIN
                 <FaLongArrowAltRight />
               </Center>
             </StyledButton>
           </form>
           <div>
-          <Link to = '/Login'>
-            <StyledParagraph color="#707070" fontSize="15px"  margin = "35px">Already member? <span>
-              Sign in</span>
-              </StyledParagraph>
-              </Link>
+            <Link to ="/SignUp">
+            <StyledParagraph color="#707070" fontSize="15px" margin = "35px">Don't have account? <span>Sign up </span></StyledParagraph>
+
+            </Link>
           </div>
         </div>
       </RightSectionSign>
